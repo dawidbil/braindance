7b955cfe2978039469627bed12b6074c89c0ff38
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-17 22:45:05 +0100

    use Vector3 inside Color struct and delegate some of its functions; update lerp

--- src/color.rs
+++ src/color.rs
@@ -14,9 +14,7 @@ fn check_color_value(x: f64) {
 
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Color {
-    pub r: f64,
-    pub g: f64,
-    pub b: f64,
+    vec: Vector3
 }
 
 impl Color {
@@ -26,24 +24,63 @@ impl Color {
         check_color_value(r);
         check_color_value(g);
         check_color_value(b);
-        Color { r, g, b }
+        Color { vec: Vector3::new(r, g, b) }
     }
 
+    pub fn r(&self) -> f64 { self.vec.x }
+    pub fn g(&self) -> f64 { self.vec.y }
+    pub fn b(&self) -> f64 { self.vec.z }
+
+    pub fn add(&self, other: &Color) -> Self {
+        Self { vec: self.vec.add(&other.vec) }
+    }
+
+    pub fn sub(&self, other: &Color) -> Self {
+        Self { vec: self.vec.sub(&other.vec) }
+    }
+
+    pub fn mul(&self, scalar: f64) -> Self {
+        Self { vec: self.vec.mul(scalar) }
+    }
+
+    pub fn div(&self, scalar: f64) -> Self {
+        Self { vec: self.vec.div(scalar) }
+    }
+
+    /// Convert from Vector3
     pub fn from_vec(v: Vector3) -> Self {
         Self::new(v.x, v.y, v.z)
+    }
+
+    /// Convert to Vector3
+    pub fn as_vec(&self) -> &Vector3 {
+        &self.vec
     }
 
     /// Dump the color to a writer.
     /// Scales the color to the range 0-255 and writes it to the writer.
     pub fn dump<T: Write>(&self, out: &mut T) -> Result<(), std::io::Error> {
-        write!(out, "{} {} {}\n", to_256(self.r), to_256(self.g), to_256(self.b))
+        write!(out, "{} {} {}\n", 
+            to_256(self.r()), 
+            to_256(self.g()), 
+            to_256(self.b()))
+    }
+}
+
+// Implement From for clean conversions
+impl From<Vector3> for Color {
+    fn from(vec: Vector3) -> Self {
+        Self::from_vec(vec)
     }
 }
 
 impl Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Color({} {} {})", self.r, self.g, self.b)?;
-        write!(f, "Color({} {} {})", to_256(self.r), to_256(self.g), to_256(self.b))?;
+        write!(f, "Color({} {} {})", self.r(), self.g(), self.b())?;
+        write!(f, " RGB({} {} {})", 
+            to_256(self.r()), 
+            to_256(self.g()), 
+            to_256(self.b()))?;
         Ok(())
     }
 }
