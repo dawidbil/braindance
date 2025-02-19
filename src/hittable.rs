c72d010e0b71094276cda24f6238ded29afdd0e6
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-19 02:13:44 +0100

    add hittable objects and sphere as hittable object

---
+++ src/hittable.rs
@@ -0,0 +1,27 @@
+use crate::ray::Ray;
+use crate::vector3::{Point3, Vector3};
+
+#[derive(Debug)]
+pub struct HitRecord {
+    pub point: Point3,
+    pub normal: Vector3,
+    pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new(point: Point3, outward_normal: Vector3, t: f64, ray_direction: Vector3) -> Self {
+        let front_face = ray_direction.dot(&outward_normal) < 0.0;
+        let normal = if front_face { outward_normal } else { outward_normal.neg() };
+        HitRecord {
+            point,
+            normal,
+            t,
+            front_face,
+        }
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord>;
+} 
---
+++ src/sphere.rs
@@ -0,0 +1,57 @@
+use crate::hittable::{Hittable, HitRecord};
+use crate::ray::Ray;
+use crate::vector3::Point3;
+use crate::assert_near_eq;
+
+#[derive(Debug)]
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        if radius <= 0.0 {
+            panic!("Radius must be positive");
+        }
+        Sphere { center, radius }
+    }
+
+    fn is_t_valid(&self, t: f64, ray_tmin: f64, ray_tmax: f64) -> bool {
+        t > ray_tmin && t < ray_tmax
+    }
+
+    fn get_hit_record(&self, ray: &Ray, t: f64) -> HitRecord {
+        let point = ray.at(t);
+        assert_near_eq!(point.sub(&self.center).length(), self.radius);
+        let outward_normal = point.sub(&self.center).div(self.radius);
+        HitRecord::new(point, outward_normal, t, ray.direction)
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        let oc = self.center.sub(&ray.origin);
+        let a = ray.direction.dot(&ray.direction);
+        let h = ray.direction.dot(&oc);
+        let c = oc.dot(&oc) - self.radius * self.radius;
+        let discriminant = h * h - a * c;
+
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+        let mut root = (h - sqrtd) / a;
+        if self.is_t_valid(root, ray_tmin, ray_tmax) {
+            return Some(self.get_hit_record(ray, root));
+        }
+
+        root = (h + sqrtd) / a;
+        if self.is_t_valid(root, ray_tmin, ray_tmax) {
+            return Some(self.get_hit_record(ray, root));
+        }
+
+        None
+    }
+} 
