55e0f35591aedfe14a4641225f8d5f0b95612e04
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-19 02:13:23 +0100

    rename viewport to camera and add functionality

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,14 @@
+use std::fs::File;
+use std::io::{Write, BufWriter};
+
 use crate::vector3::{Point3, Vector3};
+use crate::color::Color;
+use crate::ray::Ray;
+use crate::hittables::Hittables;
+use crate::utils::lerp;
 
 #[derive(Debug)]
-pub struct Viewport {
+pub struct Camera {
     pub aspect_ratio: f64,
     pub image_width: u32,
     pub image_height: u32,
@@ -17,7 +24,7 @@ pub struct Viewport {
     pub pixel_upper_left: Point3,
 }
 
-impl Viewport {
+impl Camera {
     pub fn new(aspect_ratio: f64, image_width: u32, viewport_height: f64, focal_length: f64, camera_center: Point3) -> Self {
         let image_height = match (image_width as f64 / aspect_ratio) as u32 {
             0 => 1,
@@ -48,9 +55,37 @@ impl Viewport {
         }
     }
 
-    pub fn pixel_center(&self, i: u32, j: u32) -> Point3 {
+    fn pixel_center(&self, i: u32, j: u32) -> Point3 {
         let pixel_delta_u_i = self.pixel_delta_u.mul(i as f64);
         let pixel_delta_v_j = self.pixel_delta_v.mul(j as f64);
         self.pixel_upper_left.add(&pixel_delta_u_i).add(&pixel_delta_v_j)
     }
-}
+
+    fn ray_color(&self, ray: &Ray, hittables: &Hittables) -> Color {
+        if let Some(hit) = hittables.hit(ray, 0.0, f64::INFINITY) {
+            let normal = hit.normal;
+            return Vector3::new(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0).mul(0.5).to_color();
+        }
+    
+        let unit_direction = ray.direction.normalize();
+        let t = 0.5 * (unit_direction.y + 1.0);
+        lerp(Color::new(1.0, 1.0, 1.0), Color::new(0.5, 0.7, 1.0), t)
+    }
+
+    pub fn render(&self, writer: &mut BufWriter<File>, hittables: &Hittables) -> Result<(), std::io::Error> {
+        write!(writer, "P3\n{} {}\n255\n", self.image_width, self.image_height)?;
+    
+        for j in 0..self.image_height {
+            print!("\rScanlines remaining: {:>4}", self.image_height - j);
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel_center(i, j);
+                let ray_direction = pixel_center.sub(&self.camera_center);
+                let ray = Ray::new(self.camera_center, ray_direction);
+                let color = self.ray_color(&ray, &hittables);
+                color.dump(writer)?;
+            }
+        }
+        println!("\r{:-^30}", "Done");
+        Ok(())
+    }
+} 
