12bfb7af6ca032fffb85989e185442d2413225a3
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-19 02:14:07 +0100

    add hittables list

---
+++ src/hittables.rs
@@ -0,0 +1,30 @@
+use crate::hittable::{Hittable, HitRecord};
+use crate::ray::Ray;
+
+pub struct Hittables {
+    pub hittables: Vec<Box<dyn Hittable>>,
+}
+
+impl Hittables {
+    pub fn new() -> Self {
+        Hittables { hittables: Vec::new() }
+    }
+
+    pub fn add(&mut self, hittable: Box<dyn Hittable>) {
+        self.hittables.push(hittable);
+    }
+
+    pub fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        let mut hit_record = None;
+        let mut closest_so_far = ray_tmax;
+
+        for hittable in self.hittables.iter() { 
+            if let Some(record) = hittable.hit(ray, ray_tmin, closest_so_far) {
+                closest_so_far = record.t;
+                hit_record = Some(record);
+            }
+        }
+
+        hit_record
+    }
+}
