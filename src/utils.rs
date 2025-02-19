2df41e469359dbe1f348fa97aa714ca7ed023a5b
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-19 02:12:01 +0100

    add macro to compare floats

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,15 @@
 use crate::color::Color;
 
+#[macro_export]
+macro_rules! assert_near_eq {
+    ($a:expr, $b:expr) => {
+        assert!(($a - $b).abs() < 1e-10, "Expected {} to be near {}", $a, $b);
+    };
+    ($a:expr, $b:expr, $epsilon:expr) => {
+        assert!(($a - $b).abs() < $epsilon, "Expected {} to be near {} (within {})", $a, $b, $epsilon);
+    };
+}
+
 pub fn lerp(a: Color, b: Color, t: f64) -> Color {
     Color::new(
         a.r() * (1.0 - t) + b.r() * t,
