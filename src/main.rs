7f0f8d3a8112a54766ff482af81e8424c4fc52b5
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-19 02:14:24 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,27 @@
+#![allow(dead_code)]
+
 use std::fs::File;
-use std::io::{Write, BufWriter};
+use std::io::BufWriter;
 
 mod vector3;
-use vector3::{Point3, Vector3};
+use vector3::Point3;
 
 mod color;
-use color::Color;
-
-mod viewport;
-use viewport::Viewport;
-
+mod camera;
+use camera::Camera;
 mod ray;
-use ray::Ray;
-
 mod utils;
-use utils::lerp;
+mod sphere;
+use sphere::Sphere;
+mod hittables;
+use hittables::Hittables;
+mod hittable;
 
-const IMAGE_WIDTH: u32 = 256;
+const IMAGE_WIDTH: u32 = 1600;
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const VIEWPORT_HEIGHT: f64 = 2.0;
 const FOCAL_LENGTH: f64 = 1.0;
 const CAMERA_CENTER_TUPLE: (f64, f64, f64) = (0.0, 0.0, 0.0);
-
-fn ray_color(ray: &Ray) -> Color {
-    let unit_direction = ray.direction.normalize();
-    let t = 0.5 * (unit_direction.y + 1.0);
-    lerp(Color::new(1.0, 1.0, 1.0), Color::new(0.5, 0.7, 1.0), t)
-}
-
-fn render(viewport: &Viewport, writer: &mut BufWriter<File>) -> Result<(), std::io::Error> {
-    write!(writer, "P3\n{} {}\n255\n", viewport.image_width, viewport.image_height)?;
-
-    for j in 0..viewport.image_height {
-        println!("\rScanlines remaining: {}", viewport.image_height - j);
-        for i in 0..viewport.image_width {
-            let pixel_center = viewport.pixel_center(i, j);
-            let ray_direction = pixel_center.sub(&viewport.camera_center);
-            let ray = Ray::new(viewport.camera_center, ray_direction);
-            let color = ray_color(&ray);
-            color.dump(writer)?;
-        }
-    }
-    println!("\rDone");
-    Ok(())
-}
 
 fn main() {
     let camera_center = Point3::new(
@@ -51,15 +29,18 @@ fn main() {
         CAMERA_CENTER_TUPLE.1,
         CAMERA_CENTER_TUPLE.2,
     );
-    let viewport = Viewport::new(
+    let camera = Camera::new(
         ASPECT_RATIO,
         IMAGE_WIDTH,
         VIEWPORT_HEIGHT,
         FOCAL_LENGTH,
         camera_center,
     );
+    // println!("Camera: {:#?}", camera);
+    let mut hittables = Hittables::new();
+    hittables.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    hittables.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
     let file = File::create("image.ppm").unwrap();
     let mut writer = BufWriter::new(file);
-    render(&viewport, &mut writer).expect("Failed to dump image");
-    println!("Viewport: {:#?}", viewport);
+    camera.render(&mut writer, &hittables).expect("Failed to dump image");
 }
