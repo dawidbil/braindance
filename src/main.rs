55f185d7ed44da856eb3209839ec294c22ffbf2a
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-22 22:35:38 +0100

    add new imports, materials and balls

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,22 @@ mod ray;
 mod sphere;
 mod utils;
 mod vector3;
-
+mod material;
+mod lambertian;
+mod metal;
 use camera::Camera;
 use hittables::Hittables;
 use sphere::Sphere;
 use vector3::Point3;
+use lambertian::Lambertian;
+use metal::Metal;
+use color::Color;
 
 const IMAGE_WIDTH: u32 = 512;
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const VIEWPORT_HEIGHT: f64 = 2.0;
 const FOCAL_LENGTH: f64 = 1.0;
-const SAMPLES_PER_PIXEL: u32 = 10;
+const SAMPLES_PER_PIXEL: u32 = 50;
 const MAX_DEPTH: u32 = 50;
 const CAMERA_CENTER_TUPLE: (f64, f64, f64) = (0.0, 0.0, 0.0);
 
@@ -40,9 +45,16 @@ fn main() {
         camera_center,
     );
     // println!("Camera: {:#?}", camera);
+    let material_ground = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+
     let mut hittables = Hittables::new();
-    hittables.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
-    hittables.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    hittables.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
+    hittables.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.6), 0.5, material_center)));
+    hittables.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
+    hittables.add(Box::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right)));
     let file = File::create("image.ppm").unwrap();
     let mut writer = BufWriter::new(file);
     camera
