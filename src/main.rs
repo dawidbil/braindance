a9c328e4e882e927c7c824c931f208cd26ff4be2
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-26 00:40:57 +0100

    add more complicated scene

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use std::fs::File;
 use std::io::BufWriter;
+use rand::Rng;
 
 mod camera;
 mod color;
@@ -25,17 +26,17 @@ use metal::Metal;
 use color::Color;
 use dielectric::Dielectric;
 
-const IMAGE_WIDTH: u32 = 512;
+const IMAGE_WIDTH: u32 = 1200;
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const VFOV: f64 = 20.0;
-const SAMPLES_PER_PIXEL: u32 = 100;
+const SAMPLES_PER_PIXEL: u32 = 20;
 const MAX_DEPTH: u32 = 50;
-const DEFOCUS_ANGLE: f64 = 10.0;
-const FOCUS_DISTANCE: f64 = 3.4;
+const DEFOCUS_ANGLE: f64 = 0.6;
+const FOCUS_DISTANCE: f64 = 10.0;
 
 fn main() {
-    let lookfrom = Point3::new(-2.0, 2.0, 1.0);
-    let lookat = Point3::new(0.0, 0.0, -1.0);
+    let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
     let vup = Vector3::new(0.0, 1.0, 0.0);
     let camera = Camera::new(
         ASPECT_RATIO,
@@ -49,18 +50,41 @@ fn main() {
         DEFOCUS_ANGLE,
         FOCUS_DISTANCE,
     );
-    let material_ground = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Box::new(Dielectric::new(1.5));
-    let material_bubble = Box::new(Dielectric::new(1.0 / 1.5));
-    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
-
     let mut hittables = Hittables::new();
-    hittables.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
-    hittables.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.2), 0.5, material_center)));
-    hittables.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
-    hittables.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.4, material_bubble)));
-    hittables.add(Box::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right)));
+
+    let ground_material = Box::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    hittables.add(Box::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rand::thread_rng().gen_range(0.0..1.0);
+            let center = Point3::new(a as f64 + 0.9 * rand::thread_rng().gen_range(0.0..1.0), 0.2, b as f64 + 0.9 * rand::thread_rng().gen_range(0.0..1.0));
+            if center.sub(&Vector3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    let albedo = Vector3::random(0.0, 1.0).to_color();
+                    let sphere_material = Box::new(Lambertian::new(albedo));
+                    hittables.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    let albedo = Vector3::random(0.5, 1.0).to_color();
+                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
+                    let sphere_material = Box::new(Metal::new(albedo, fuzz));
+                    hittables.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    let sphere_material = Box::new(Dielectric::new(1.5));
+                    hittables.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Box::new(Dielectric::new(1.5));
+    hittables.add(Box::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, material1)));
+
+    let material2 = Box::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    hittables.add(Box::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, material2)));
+
+    let material3 = Box::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    hittables.add(Box::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, material3)));
 
     let file = File::create("image.ppm").unwrap();
     let mut writer = BufWriter::new(file);
