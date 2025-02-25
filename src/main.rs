bafc776cb13730c274df3380060b297d10c00c9f
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-02-25 22:59:06 +0100

    add bubble inside left ball

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ const IMAGE_WIDTH: u32 = 512;
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const VIEWPORT_HEIGHT: f64 = 2.0;
 const FOCAL_LENGTH: f64 = 1.0;
-const SAMPLES_PER_PIXEL: u32 = 50;
+const SAMPLES_PER_PIXEL: u32 = 100;
 const MAX_DEPTH: u32 = 50;
 const CAMERA_CENTER_TUPLE: (f64, f64, f64) = (0.0, 0.0, 0.0);
 
@@ -50,13 +50,15 @@ fn main() {
     // println!("Camera: {:#?}", camera);
     let material_ground = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Box::new(Dielectric::new(1.0 / 1.33));
+    let material_left = Box::new(Dielectric::new(1.5));
+    let material_bubble = Box::new(Dielectric::new(1.0 / 1.5));
     let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     let mut hittables = Hittables::new();
     hittables.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
     hittables.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.2), 0.5, material_center)));
     hittables.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
+    hittables.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.4, material_bubble)));
     hittables.add(Box::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right)));
     let file = File::create("image.ppm").unwrap();
     let mut writer = BufWriter::new(file);
